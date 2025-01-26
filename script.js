fb48410ebd450a31a0b478b926e782de2b333d1d
guialmeida10 <guisardinha09@gmail.com>
2025-01-26 13:12:58 -0300

    Atualização no site com novos recursos e layout

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if(html.classList.contains("light")) {
-   // se tiver light mode, adicionar a imagem light
-   img.setAttribute("src", "./assets/avatar-light.png")
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
- 
 }
